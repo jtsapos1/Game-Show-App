ba9063195db1193292fd3d23eb7069df3d6103e0
jtsapos <jtsapos@gmx.com>
2022-11-01 15:18:09 -0400

    Created app.js and added variables

---
+++ app.js
@@ -0,0 +1,5 @@
+const qwerty = document.getElementById (qwerty);
+const phrase = document.getElementById (phrase);
+const btnreset = document.querySelector ('.btn_reset');
+let correctAnswers = 0;
+
