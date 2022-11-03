3cd4140c089e2f97174bf2d581bcf5b44e8c9d64
jtsapos <jtsapos@gmx.com>
2022-11-03 13:51:14 -0400

    Updated app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
 const qwerty = document.getElementById ('qwerty');
 const phrase = document.getElementById ('phrase');
 const resetButton = document.querySelector ('.btn_reset');
-let correctAnswers = 0;
+let missed = 0;
 
 const phrases = [
     'washington', 
@@ -17,18 +17,18 @@ resetButton.addEventListener('click', () => {
 });  
 
 //return a random phrase from an array
-getRandomPhraseAsArray = (array) => {
-    const randomNumber = array[Math.floor(Math.random() * array.length)];
+function getRandomPhraseAsArray(arr) {
+    const randomNumber = arr[Math.floor(Math.random() * arr.length)];
     return randomNumber;
 }
 
 //add the letters of a string to the display
-addPhraseToDisplay = (array) => {
-    for (let i = 0; i < array.length; i++)
+function addPhraseToDisplay(arr) {
+    for (let i = 0; i < arr.length; i++)
     const li = document.createElement('li');//creates the li.
-    li.textContent = array[i]; //puts the character inside the li.
+    li.textContent = arr[i]; //puts the character inside the li.
     ul.appendChild(li);//append the li to the #phrase ul.
-    if (array[i] !== '') {
+    if (arr[i] !== '') {
         li.className = 'letter';
     } else {
         li.classname = 'space';
@@ -37,4 +37,16 @@ addPhraseToDisplay = (array) => {
 
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
+
+// check if letter is in the phrase
+const checkletter = button => {
+    
+
+
+} 
+
+
+
+
+
 }
